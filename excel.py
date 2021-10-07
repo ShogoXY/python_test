df3aeef6524f81e51f594c31c7ebb93495e0f4dd
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-10-07 13:58:10 +0200

    Update excel.py

--- excel.py
+++ excel.py
@@ -2,6 +2,7 @@
 import openpyxl
 import os
 import msvcrt
+import keyboard
 os.chdir("C:\\Users\\Dariusz\\Desktop\\Śmieci\\python")
 
 # Path
@@ -9,24 +10,21 @@ wb = openpyxl.load_workbook('test1.xlsx')
 
 # active worksheet data
 ws = wb.active
+print("\nBy wyjść naciśniej ESC+ENTER\n")
 print("podaj komentarz do wpisania")
-komentarz = input()
+commet = input()
 
-
-#serial = (input("podaj wartość którą chcesz wyszukać\n"))
-#print("by wyjść nie podawaj nic i wciśnij ENTER\n")
-
-#    serial = input("podaj wartość którą chcesz wyszukać\n")
-while True:
+while 1:
     def wordfinder(searchString):
         for i in range(1, ws.max_row + 1):
             for j in range(1, ws.max_column + 1):
                 if searchString == ws.cell(i, j).value:
                     print(ws.cell(i, j))
                     test = ws.cell(i, 6)
-                    test.value = komentarz
-    print("podaj wartość którą chcesz wyszukać")
-    wordfinder(input())
-    wb.save('test1.xlsx')
-else:
-    break
+                    test.value = commet
+
+    wordfinder(input("podaj wartość którą chcesz wyszukać\n"))
+    if keyboard.is_pressed("esc"):
+        wb.save('test1.xlsx')
+        print ("zapisane wychodzę")
+        break
