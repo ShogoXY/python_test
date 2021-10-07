6f18988ab129189cd3d00564e89cdf87a0cf2d8e
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2021-10-07 15:40:37 +0200

    Update excel2.py

--- excel2.py
+++ excel2.py
@@ -1,26 +1,33 @@
 # dobra to teraz testujemy
 import openpyxl
 import os
-os.chdir("/home/fedora/python_test")
+import msvcrt
+import keyboard
+os.chdir("C:\\Users\\Dariusz\\Desktop\\Śmieci\\python")
 
 # Path
 wb = openpyxl.load_workbook('test1.xlsx')
 
 # active worksheet data
 ws = wb.active
+print("\nBy wyjść naciśniej ESC+ENTER\n")
 print("podaj komentarz do wpisania")
-komentarz = input()
+commet = input()
 
+while 1:
+    def wordfinder(searchString):
+        for i in range(1, ws.max_row + 1):
+            for j in range(1, ws.max_column + 1):
+                if searchString == ws.cell(i, j).value:
+                    print(ws.cell(i, j))
+                    test = ws.cell(i, 6)
+                    test.value = commet
+                
+    search_word=input("podaj wartość którą chcesz wyszukać\n")
+    wordfinder(search_word)
 
-def wordfinder(searchString):
-    for i in range(1, ws.max_row + 1):
-        for j in range(1, ws.max_column + 1):
-            if searchString == ws.cell(i, j).value:
-                print(ws.cell(i, j))
-                test = ws.cell(i, 6)
-                test.value = komentarz
+    if search_word == "":
+        wb.save('test1.xlsx')
+        print ("zapisane wychodzę")
+        break
 
-
-print("podaj wartość którą chcesz wyszukać")
-wordfinder(input())
-wb.save('test1.xlsx')
