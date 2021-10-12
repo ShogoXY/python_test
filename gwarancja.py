25d583b7a2ba1e3c576d9df82c24c1e41ced7c19
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-12 18:17:30 +0200

    no dobra udało się cos zrobić

--- gwarancja.py
+++ gwarancja.py
@@ -1,12 +1,12 @@
 # program do tworzenia karty gwarancyjnej w PDF na podstawie danych z Excel
 import os
-#import pandas as pd
-#import docxptl
-#import docx
+# import pandas as pd
+# import docxptl
+# import docx
 import openpyxl
 import sys
 
-os.chdir("/home/fedora/git/python_test")
+os.chdir("/home/darek/git/python_test")
 wb = openpyxl.load_workbook('test.xlsx')
 ws = wb.active
 
@@ -21,14 +21,12 @@ def wordfinder(searchString):
         if searchString == ws.cell(i, 5).value:
             commet = ws.cell(i, 11).value
             print(commet)
-
-
-def copy_to_sh(commet):
-    for i in range(1, ws.max_row + 1):
-        if commet == ws.cell(i, 11).value:
-            pn = ws.cell(i, 4).value
-            sn = ws.cell(i, 5).value
-            print(sn, pn)
+            for i in range(1, ws.max_row + 1):
+                if commet == ws.cell(i, 11).value:
+                    pn = ws.cell(i, 4).value
+                    sn = ws.cell(i, 5).value
+                    print(sn, pn)
 
 
 wordfinder(search_word)
+# wordfinder(commet)
