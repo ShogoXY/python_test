7fa643c491301710c97c4b22eb9ee11834c8b103
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-13 14:18:44 +0200

    działajacy program wpisujący dane do nowego arkusza

--- gwarancja.py
+++ gwarancja.py
@@ -6,32 +6,31 @@ import os
 import openpyxl
 import sys
 
-os.chdir("/home/darek/git/python_test")
+os.chdir("/home/fedora/git/python_test")
 wb = openpyxl.load_workbook('test.xlsx')
 ws = wb['Arkusz1']
-# sn_warrany=input("Podaj numer seryjny do druku gwarancji\n")
 if 'dane' not in wb.sheetnames:
     wb.create_sheet('dane')
 ws_1 = wb['dane']
+
 
 searchString = input("podaj wartość którą chcesz wyszukać\n")
 for i in range(1, ws.max_row + 1):
     if searchString == ws.cell(i, 5).value:
         commet = ws.cell(i, 11).value
-        print(commet)
         for i in range(1, ws.max_row + 1):
             if commet == ws.cell(i, 11).value:
                 pn = ws.cell(i, 4).value
                 sn = ws.cell(i, 5).value
-                gwara1 = str(pn)
-                gwara2 = str(sn)
-                full_gwara = (gwara1 + ", " + gwara2)
-                print(full_gwara)
-            for j in range(1, ws_1.max_row + 1):
-                ws_1.cell(j + 1, 1).value = gwara1
-                ws_1.cell(j + 1, 2).value = gwara2
+                gwara = ("Produkt --  " + pn + " \nSeryjny --  " + sn + "\n")
+                # print(gwara)
+                ws_1.cell(i, 1).value = pn
+                ws_1.cell(i, 2).value = sn
 
-            #    ws_1.cell(j, 2).value = sn
-# print(len(gwara))
-# Save the openpyxl Workbook object to file
+count = 0
+for row in ws_1:
+    if all([cell.value == None for cell in row]):
+        count += 1
+ws_1.delete_rows(1, count)
+print(gwara)
 wb.save('test.xlsx')
