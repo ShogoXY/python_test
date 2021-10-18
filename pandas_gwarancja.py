6a01d6258fd312ff2808c242a0c553ed95246228
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-18 12:55:02 +0200

    poprawny już działajcy

--- pandas_gwarancja.py
+++ pandas_gwarancja.py
@@ -1,23 +1,30 @@
 # bedziemy próbować zrobić działający program na plikach excel za pomocą Pandas
 import pandas as pd
 import numpy as np
-import openpyxl
-#wb = openpyxl.load_workbook('/home/darek/python/test.xlsx')
-df = pd.read_excel('/home/darek/python/test.xlsx')
+from openpyxl import load_workbook
+
+file = "/home/fedora/git/python_test/test.xlsx"
+
+df = pd.read_excel(file, sheet_name='Produkcja')
 
 print(df.head())
 
-search_word = input()
+search_word = input("podaj numer seryjny do wyszukania \n")
 
-ppd = df.loc[df['nazwa'] == search_word]
+ppd = df.loc[df['NUMER SERYJNY'] == search_word]
 
-s_ddp = ppd['cena'].values[0]
+s_ddp = ppd['KOMENTARZ'].values[0]
 print(s_ddp)
-p_ddp = df.loc[df['cena'] == s_ddp]
+p_ddp = df.loc[df['KOMENTARZ'] == s_ddp]
 print(p_ddp)
 
 
-df1 = pd.DataFrame(p_ddp)
-df1.to_excel("/home/darek/python/test.xlsx", sheet_name='test2')
+excel_book = load_workbook(file)
+writer = pd.ExcelWriter(file, engine='openpyxl',mode='a', if_sheet_exists='replace')
+writer.book = excel_book
+writer.sheets = dict((ws.title, ws) for ws in excel_book.worksheets)
 
-# wb.save('/home/darek/python/test.xlsx')
+df1 = pd.DataFrame(p_ddp, columns=['NAZWA', 'NUMER SERYJNY'])
+df1.to_excel(writer, sheet_name='arkusz')
+writer.save()
+writer.close()
