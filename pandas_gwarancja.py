d8fc40c6b61eadfdbdc3f429c2ca85ec10f764cd
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-18 15:22:57 +0200

    poprawiony jeszcze raz

--- pandas_gwarancja.py
+++ pandas_gwarancja.py
@@ -2,29 +2,29 @@
 import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
-
+# ścieżka dostępu do pliku
 path_file = "/home/fedora/git/python_test/test.xlsx"
 
+# wyszukuje wartości w akruszu (czy jest, jeśli tak wypisuje ją)
 df = pd.read_excel(path_file, sheet_name='Produkcja')
-
-print(df.head())
-
 search_word = input("podaj numer seryjny do wyszukania \n")
-
 search_df = df.loc[df['NUMER SERYJNY'] == search_word]
 
+# wypisuje warość przypisaną do szukanej a następnie wypisuje na tej podstawie tabelę
 komentarz_search = search_df['KOMENTARZ'].values[0]
 print(komentarz_search)
 komentarz_df = df.loc[df['KOMENTARZ'] == komentarz_search]
 print(komentarz_df)
 
-
+# wpisuje wyszukiwane wartości do nowego arkusza, nadpisując je
 excel_book = load_workbook(path_file)
-writer = pd.ExcelWriter(path_file, engine='openpyxl', mode='a', if_sheet_exists='replace')
+writer = pd.ExcelWriter(path_file, engine='openpyxl',
+                        mode='a', if_sheet_exists='replace')
 writer.book = excel_book
 writer.sheets = dict((ws.title, ws) for ws in excel_book.worksheets)
-
 df1 = pd.DataFrame(komentarz_df, columns=['NAZWA', 'NUMER SERYJNY'])
 df1.to_excel(writer, sheet_name='arkusz')
+
+# zapisz wyjdź
 writer.save()
 writer.close()
