3f480bf74ad3567bd753b470a7ebf1d81903c020
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-18 14:05:50 +0200

    poprawiony

--- pandas_gwarancja.py
+++ pandas_gwarancja.py
@@ -3,28 +3,28 @@ import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
 
-file = "/home/fedora/git/python_test/test.xlsx"
+path_file = "/home/fedora/git/python_test/test.xlsx"
 
-df = pd.read_excel(file, sheet_name='Produkcja')
+df = pd.read_excel(path_file, sheet_name='Produkcja')
 
 print(df.head())
 
 search_word = input("podaj numer seryjny do wyszukania \n")
 
-ppd = df.loc[df['NUMER SERYJNY'] == search_word]
+search_df = df.loc[df['NUMER SERYJNY'] == search_word]
 
-s_ddp = ppd['KOMENTARZ'].values[0]
-print(s_ddp)
-p_ddp = df.loc[df['KOMENTARZ'] == s_ddp]
-print(p_ddp)
+komentarz_search = search_df['KOMENTARZ'].values[0]
+print(komentarz_search)
+komentarz_df = df.loc[df['KOMENTARZ'] == komentarz_search]
+print(komentarz_df)
 
 
-excel_book = load_workbook(file)
-writer = pd.ExcelWriter(file, engine='openpyxl',mode='a', if_sheet_exists='replace')
+excel_book = load_workbook(path_file)
+writer = pd.ExcelWriter(path_file, engine='openpyxl', mode='a', if_sheet_exists='replace')
 writer.book = excel_book
 writer.sheets = dict((ws.title, ws) for ws in excel_book.worksheets)
 
-df1 = pd.DataFrame(p_ddp, columns=['NAZWA', 'NUMER SERYJNY'])
+df1 = pd.DataFrame(komentarz_df, columns=['NAZWA', 'NUMER SERYJNY'])
 df1.to_excel(writer, sheet_name='arkusz')
 writer.save()
 writer.close()
