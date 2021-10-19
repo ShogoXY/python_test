28e9611c8bf982d470ca6c53a30b7f1ce050ecc7
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-10-19 16:10:15 +0200

    test

--- pandas_gwarancja.py
+++ pandas_gwarancja.py
@@ -1,27 +1,29 @@
 # bedziemy próbować zrobić działający program na plikach excel za pomocą Pandas
 import pandas as pd
 import docx
+from docx import Document
+from docxtpl import DocxTemplate, InlineImage
 import numpy as np
 from openpyxl import load_workbook
 
-# ścieżka dostępu do pliku
-path_file = "/home/fedora/git/python_test/test.xlsx"
-path_docx = "/home/fedora/git/python_test/word1.docx"
+# file path
+path_file = "C:\\Users\\Dariusz\\github\\python_test\\test.xlsx"
+path_docx = "C:\\Users\\Dariusz\\github\\python_test\\word1.docx"
 
 doc = docx.Document(path_docx)
 
-# wyszukuje wartości w akruszu (czy jest, jeśli tak wypisuje ją)
+# search value in excel sheet (if yes, pritn it)
 df = pd.read_excel(path_file, sheet_name='Produkcja')
 search_word = input("podaj numer seryjny do wyszukania \n")
 search_df = df.loc[df['NUMER SERYJNY'] == search_word]
 
-# wypisuje warość przypisaną do szukanej a następnie wypisuje na tej podstawie tabelę
+# print value in specific column and search with this value
 komentarz_search = search_df['KOMENTARZ'].values[0]
 print(komentarz_search)
 komentarz_df = df.loc[df['KOMENTARZ'] == komentarz_search]
 print(komentarz_df)
 
-# wpisuje wyszukiwane wartości do nowego arkusza, nadpisując je
+# write value to new sheet in same workbook
 excel_book = load_workbook(path_file)
 writer = pd.ExcelWriter(path_file, engine='openpyxl',
                         mode='a', if_sheet_exists='replace')
@@ -30,17 +32,35 @@ writer.sheets = dict((ws.title, ws) for ws in excel_book.worksheets)
 df1 = pd.DataFrame(komentarz_df, columns=['NAZWA', 'NUMER SERYJNY'])
 df1.to_excel(writer, sheet_name='arkusz')
 
-# zapisz wyjdź
+# save nd exit excel
 writer.save()
 writer.close()
 
-t = doc.add_table(df1.shape[0]+1, df1.shape[1])
+
+# word create table in word
+table = doc.add_table(df1.shape[0]+1, df1.shape[1], style='Table Grid')
+
 
 for j in range(df1.shape[-1]):
-    t.cell(0, j).text = df1.columns[j]
+    table.cell(0, j).text = df1.columns[j]
 
 for i in range(df1.shape[0]):
     for j in range(df1.shape[-1]):
-        t.cell(i+1, j).text = str(df1.values[i, j])
+        table.cell(i+1, j).text = str(df1.values[i, j])
 
+
+print(str(df1.values[i, j]))
 doc.save(path_docx)
+
+
+input = Document('word1.docx')
+
+paragraphs = []
+for para in input.paragraphs:
+    p = para.text
+    paragraphs.append(p)
+
+output = Document()
+for item in paragraphs:
+    output.add_paragraph(item)
+output.save('OutputDoc.docx')
