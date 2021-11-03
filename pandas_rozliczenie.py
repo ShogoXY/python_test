51dc0a38d44c19ba269eda7530f1a66e20e4c362
ShogoXY <dariusz.kadzielawa@protonmail.com>
2021-11-03 14:54:43 +0100

    dodane wyszukiwnie

--- pandas_rozliczenie.py
+++ pandas_rozliczenie.py
@@ -5,6 +5,7 @@ from docx import Document
 from docxtpl import DocxTemplate, InlineImage
 import numpy as np
 import openpyxl
+import re
 # from openpyxl import load_workbook
 
 import datetime
@@ -13,8 +14,10 @@ from re import search
 import keyboard
 
 # file path
-path_file = "C:\\Users\\Dariusz\\github\\python_test\\serwi.xlsx"
+path_excel_open = "C:\\Users\\Dariusz\\github\\python_test\\serwi.xlsx"
 path_docx = "C:\\Users\\Dariusz\\github\\python_test\\Rozliczenie serwisowe.docx"
+path_excel_save = "C:\\Users\\Dariusz\\github\\python_test\\serwi2.xlsx"
+
 
 doc = docx.Document(path_docx)
 
@@ -33,29 +36,38 @@ context = {'date': ct, 'cr_number': cr2, 'nazwa': nazwa, 'data': ct}
 doc.render(context)
 
 
-df = pd.read_excel(path_file, sheet_name='Arkusz1')
+df = pd.read_excel(path_excel_open, sheet_name='Arkusz1')
+df = df.astype(str)
 print(df)
 
 df_name = "arkusz_test"
-excel_book = openpyxl.load_workbook(path_file)
+excel_book = openpyxl.load_workbook(path_excel_save)
 
 if df_name not in excel_book.sheetnames:
     excel_book.create_sheet(df_name)
-excel_book.save(path_file)
+excel_book.save(path_excel_save)
 
-writer = pd.ExcelWriter(path_file, engine='openpyxl',
+writer = pd.ExcelWriter(path_excel_save, engine='openpyxl',
                         mode='a', if_sheet_exists='replace')
 writer.book = excel_book
 writer.sheets = dict((ws.title, ws) for ws in excel_book.worksheets)
 
 while 1:
+  
+
     def search_value(keyword, df):
         search_value = '|'.join(keyword)
         searched = df[df['RMA'].str.contains(search_value, na=False)]
         return searched
 
+
+    # podaj wartość do wpisania
     search_word = input("podaj numer RMA \n")
     search_df = search_value([search_word], df)
+    
+    
+
+
 
     if search_word == "":
         print("koniec")
@@ -65,7 +77,7 @@ while 1:
         df1 = pd.DataFrame(search_df, columns=['RMA', 'Nazwa urządzenia',
                            'Nr seryjny przyjęty', 'Nr seryjny wydany', 'UWAGI'])
         writer.save()
-        df2 = pd.read_excel(path_file, sheet_name='arkusz_test')
+        df2 = pd.read_excel(path_excel_save, sheet_name='arkusz_test')
         df3 = df2.append(df1, ignore_index=True)
         df3.to_excel(writer, sheet_name='arkusz_test', index=False)
         print(df3)
@@ -85,5 +97,5 @@ for i in range(df3.shape[0]):
 
 doc.save("rozliczenie " + cr2 + " " + nazwa+".docx")
 del excel_book[df_name]
-excel_book.save(path_file)
+excel_book.save(path_excel_save)
 excel_book.close()
